021a8611503dda8cc8f972a15e180cc5907a1e70
Zubayer Rahaman <zubayer.skd@gmail.com>
2021-07-15 20:45:05 +0600

    fixed a lot of thinsg

--- gallery/models.py
+++ gallery/models.py
@@ -7,11 +7,10 @@ class Photo(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=date.today)
-    caption = models.CharField(max_length=200, null=True, blank=True)
     image_data = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.caption
+        return self.id
 
     class Meta:
         verbose_name_plural = "Photos"
